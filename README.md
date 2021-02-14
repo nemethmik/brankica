d790c15b872a7f0a6e3b1cff8078f43015faac95
Nemeth Miklos <nemethmiklos1960@gmail.com>
2021-02-14 13:27:25 +0100

    busboy used for multipart form-data parsing

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # brankica - A NodeJS/TypeScript (node-ts) demo project to experiment with multipart mixed PDF uploading
 The project is to experiment how the multipart/form-data and multipart/mixed HTTP post bodies are to be handled and used.
 
+[Busboy](https://www.npmjs.com/package/busboy) cannot handle multipart/mixed content, unfortunately, only multipart/form-data.
+
+The [How to process POST data in Node.js?](https://stackoverflow.com/questions/4295782/how-to-process-post-data-in-node-js),
+[CURL manual for -F parameter](https://curl.se/docs/manpage.html),
+[How to read POST data?](https://nodejs.org/en/knowledge/HTTP/servers/how-to-read-POST-data/),
+[Node JS Streams, Pipes and Events](https://www.guru99.com/node-js-streams-filestream-pipes.html)
+are excellent sources of information.
+
 ## How this project was created
 - First I created a repository in Github with gitignore, readme and license.
 - I cloned this repo into a local folder with Visual Studio Code
 - Opened a terminal and run command **npm init** (main (entry point) set to build/index.js), which creted package.json
 - Then add runtime packages
-    - npm install express
+    - npm install express busboy
 - Add **development dependencies** of TypeScript, node-ts and types definitions for node and all other runtime packages
-    - npm install typescript ts-node nodemon npm-run-all @types/node @types/express --save-dev
+    - npm install typescript ts-node nodemon npm-run-all @types/node @types/express @types/busboy --save-dev
 - Configure TypeScript with **npx tsc --init** which creates a created a **tsconfig.json** file with no questions.
     - "outDir": "./build",
-    - "rootDir": "./src",
+    - "rootDir": "./src", // Create all TS files into this folder
 - Add scritps to package.json
     - "start": "node .",
     - "dev": "nodemon ./src/index.ts",
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/*
 import express = require("express")
 const app = express()
 app.get("/", (req, res) => {
@@ -6,3 +7,55 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("The application is listening on port http://localhost:3000")
 })
+*/
+import http = require("http")
+import util = require("util")
+import Busboy = require("busboy")
+const inspect = util.inspect
+ 
+http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      file.on('data', function(data) {
+        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+      });
+      file.on('end', function() {
+        console.log('File [' + fieldname + '] Finished');
+      });
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    });
+    busboy.on('finish', function() {
+      console.log('Done parsing form!');
+      res.writeHead(303, { Connection: 'close', Location: '/' });
+      res.end();
+    });
+    req.pipe(busboy);
+    // Just use regular, native processing
+    let data = ""
+    req.on("data",function(chunk){
+        data += chunk
+    })
+    req.on("end",function(){
+        console.log(req.headers)
+        console.log(data)
+        console.log('Done parsing form!');
+        res.writeHead(303, { Connection: 'close', Location: '/' });
+        res.end();
+    })
+  } else if (req.method === 'GET') {
+    res.writeHead(200, { Connection: 'close' });
+    res.end('<html><head></head><body>\
+               <form method="POST" enctype="multipart/form-data">\
+                <input type="text" name="textfield"><br />\
+                <input type="file" name="filefield"><br />\
+                <input type="submit">\
+              </form>\
+            </body></html>');
+  }
+}).listen(8000, function() {
+  console.log('Listening for requests on http://localhost:8000');
+});
